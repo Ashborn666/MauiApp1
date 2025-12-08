161e9e7524bee777dd2ac78f6f7318ddffd1b052
Ashborn666 <edrianhiraldo@gmail.com>
2025-12-07 20:25:15 -0500

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MauiApp1.Views;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MauiApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginView();
+        }
+    }
+}
